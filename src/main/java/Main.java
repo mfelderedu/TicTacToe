957b2b3c98fa234aa96215d9192297dd902a9363
mfelderedu <martin.felder@edu.teko.ch>
2019-11-21 11:15:10 +0100

    First

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Model model = new Model();
+    }
+}

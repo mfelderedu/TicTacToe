ffa00997b6641089b08449dec3b5b0996ccbf1e0
Fabian <fabian@berghirt.ch>
2020-01-04 16:20:03 +0100

    refactorings

--- src/main/java/Presenter.java
+++ src/main/java/Presenter.java
@@ -16,66 +16,36 @@ import java.util.ResourceBundle;
 
 public class Presenter implements Initializable {
     private final Game game;
-    public String activeName;
-
     private final Main main;
-
-    Map buttons = new HashMap<Character, List<Button>>();
 
     @FXML private Text active_playername;
 
-    @FXML private GridPane column_a;
-    @FXML private GridPane column_b;
-    @FXML private GridPane column_c;
-
     @FXML private GridPane field;
 
-    @FXML private Button a0;
-    @FXML private Button b0;
-    @FXML private Button c0;
-    @FXML private Button a1;
-    @FXML private Button b1;
-    @FXML private Button c1;
-    @FXML private Button a2;
-    @FXML private Button b2;
-    @FXML private Button c2;
     @FXML private Button button_newfield;
     @FXML private Button button_newgame;
     @FXML private Button button_settings;
     @FXML private Button button_exit;
 
-
-    private HashMap<String, Button> contentMap = new HashMap<String, Button>();
-    private EventHandler<ActionEvent> handler = new EventHandler<>() {
-
-        @Override
-        public void handle(ActionEvent actionEvent) {
-            // get button-id
-            Button source = (Button) actionEvent.getSource();
-            String id = source.getId();
-
-            // check the clicked field
-            String field = game.checkField(id);
-            Button pressedButton = contentMap.get(field);
-
-            //Sign p1 = Sign.cross;
-            //pressedButton.setText(String.valueOf(p1));
-
-            // set icon to the field
-            //String sign = Sign.cross;
-            Sign sign = game.getActivePlayerSign();
-            //String sign = "x";
-
-            pressedButton.setText(String.valueOf(sign.representationCharacter()));
-            game.toggleActivePlayer();
-        }
-
-    };
+    private EventHandler<ActionEvent> handler;
 
     public Presenter(Game game, Main main) {
-        //buttons.put("a",0);
         this.game = game;
         this.main = main;
+
+        handler = new EventHandler<>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Button button = (Button) actionEvent.getSource();
+                game.checkField(button.getId());
+
+                // set icon to the field
+                Sign sign = game.getActivePlayerSign();
+                button.setText(String.valueOf(sign.representationCharacter()));
+
+                game.toggleActivePlayer();
+            }
+        };
     }
 
     @Override
@@ -93,32 +63,6 @@ public class Presenter implements Initializable {
                 button.setOnAction(handler);
             }
         }
-
-        //Player activePlayer = game.getActivePlayer();
-
-
-
-        // set actions on click on field
-        a0.setOnAction(handler);
-        b0.setOnAction(handler);
-        c0.setOnAction(handler);
-        a1.setOnAction(handler);
-        b1.setOnAction(handler);
-        c1.setOnAction(handler);
-        a2.setOnAction(handler);
-        b2.setOnAction(handler);
-        c2.setOnAction(handler);
-
-        // returns sign to field
-        contentMap.put("a0", a0);
-        contentMap.put("b0", b0);
-        contentMap.put("c0", c0);
-        contentMap.put("a1", a1);
-        contentMap.put("b1", b1);
-        contentMap.put("c1", c1);
-        contentMap.put("a2", a2);
-        contentMap.put("b2", b2);
-        contentMap.put("c2", c2);
 
         button_exit.setOnAction(this::exitButtonHandler);
         button_newgame.setOnAction(this::newGameButtonHandler);
